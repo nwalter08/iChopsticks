5663dda73f1e39e13a57f628be2cfd71b7e5b863
Nick Walter <WalterMAC@new-host.home>
2012-12-30 15:50:31 -0800

    Initial Commit

---
+++ iChopsticks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iChopsticks
+//
+//  Created by Haley Walter on 12/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iChopsticks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iChopsticks
+//
+//  Created by Haley Walter on 12/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
